7db67689bcb531e710c789e7461854862d3ad5ea
Yaroslav Dolynskyi <dolynskiy13@gmail.com>
2024-02-27 17:01:37 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
 import React from 'react';
 import './index.scss';
-
+import { useState } from 'react';
 function App() {
+  const [modalOpen, setModalOpen] = useState(false);
+  const handleModal = () => {
+    setModalOpen(prev =>!prev)
+  }
+  
   return (
     <div className="App">
-      <button className="open-modal-btn">✨ Открыть окно</button>
-      {/* <div className="overlay">
+      <button onClick={handleModal} className="open-modal-btn">
+        ✨ Открыть окно
+      </button>
+
+      <div className={`overlay animated ${modalOpen ? "show " : ''}`}>
         <div className="modal">
-          <svg height="200" viewBox="0 0 200 200" width="200">
+          <svg
+            height="200"
+            viewBox="0 0 200 200"
+            width="200"
+            onClick={handleModal}
+          >
             <title />
             <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
           </svg>
-          <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
+          <img
+            src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif"
+            alt=""
+          />
         </div>
-      </div> */}
+      </div>
     </div>
   );
 }
